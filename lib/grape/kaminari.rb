012a7d042eae6a9b65d4c423554d426cd7ddb9b4
Stan Bondi <stan@fixate.it>
2014-04-01 14:56:07 +0200

    Add max value validator to parameter

--- lib/grape/kaminari.rb
+++ lib/grape/kaminari.rb
@@ -1,4 +1,5 @@
 require "grape/kaminari/version"
+require "grape/kaminari/max_value_validator"
 require "kaminari/grape"
 
 module Grape
@@ -17,12 +18,16 @@ module Grape
         end
 
         def self.paginate(options = {})
-          options.reverse_merge!(per_page: 10)
+          options.reverse_merge!(
+            per_page: 10,
+            max_per_page: false
+          )
           params do
             optional :page,     type: Integer, default: 1,
                                 desc: 'Page offset to fetch.'
             optional :per_page, type: Integer, default: options[:per_page],
-                                desc: 'Number of results to return per page.'
+                                desc: 'Number of results to return per page.',
+                                max_value: options[:max_per_page]
           end
         end
       end
