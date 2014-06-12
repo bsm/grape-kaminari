7eb872dedb521e589043ee16d473ed2c854e1b94
Stan Bondi <stan@fixate.it>
2014-06-12 15:42:31 +0200

    Use kaminari's padding instead of active records offset ref #10

--- lib/grape/kaminari.rb
+++ lib/grape/kaminari.rb
@@ -8,14 +8,13 @@ module Grape
       base.class_eval do
         helpers do
           def paginate(collection)
-            collection.page(params[:page]).per(params[:per_page]).offset(params[:offset]).tap do |data|
+            collection.page(params[:page]).per(params[:per_page]).padding(params[:offset]).tap do |data|
               header "X-Total",       data.total_count.to_s
               header "X-Total-Pages", data.num_pages.to_s
               header "X-Per-Page",    params[:per_page].to_s
               header "X-Page",        data.current_page.to_s
               header "X-Next-Page",   data.next_page.to_s
               header "X-Prev-Page",   data.prev_page.to_s
-              header "X-Page-Offset", data.offset_value.to_s
             end
           end
         end
